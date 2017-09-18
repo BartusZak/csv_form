c8f631eb8f08b2f3d3cf8627d0bd5d74cea30ca2
Bartłomiej Płoszyński <b.ploszynski@gmail.com>
2017-09-18 12:53:44 +0200

    Add files via upload
    Zoptymalizowany kod.
    Do poprawy:
    - pomyslec nad zapisywaniem plików (zmiana nagłówka),
    - pomyslec nad liczeniem kolumn,
    - czy nie napisac to obiektowo?

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-function wypisz_wszystkie_wiersze_normalnie($item,$kolumny,$filename){
+function wypisz_wszystkie_wiersze_normalnie($item,$kolumny){
     for($i=0;$i<count($item);$i++){
         for ($j=0;$j<=($kolumny-1);$j++){
             if (!isset ($item[$i][$j])){
@@ -9,11 +9,10 @@ function wypisz_wszystkie_wiersze_normalnie($item,$kolumny,$filename){
         }
         echo "\n";
     }
-    header('Content-Disposition: attachment;filename="'.$filename.'";');
 }
 
 
-function domyslny ($ilosc_powt_pierw_kolumny,$ile_wierszy_wypisac,$czy_wypisac_domain = TRUE,$kolumny,$tab,$czy_usunac_powt_pierw_kolumny = TRUE,$filename){
+function domyslny ($ilosc_powt_pierw_kolumny,$ile_wierszy_wypisac,$czy_wypisac_domain = "FALSE",$kolumny,$tab,$czy_usunac = "FALSE"){
     foreach ($tab as $item){
         if (count($item) >= $ilosc_powt_pierw_kolumny ){
             for($i=0;$i<$ile_wierszy_wypisac;$i++){
@@ -21,17 +20,18 @@ function domyslny ($ilosc_powt_pierw_kolumny,$ile_wierszy_wypisac,$czy_wypisac_d
                     if (!isset ($item[$i][$j])){
                         $item[$i][$j] = null;
                     }else {
-                        if ($czy_usunac_powt_pierw_kolumny){
-                            if($i == 0 && $j == 0 && $czy_wypisac_domain){
+                        if ($czy_usunac == "TRUE"){
+                            echo "Usuwam poet 1szej kolumny!";
+                            if($i == 0 && $j == 0 && $czy_wypisac_domain == "TRUE"){
                                 echo "domain:".$item[$i][$j].";";                             
                             }else {
-                                if ($i == 0 && $j == 0 && $czy_wypisac_domain == FALSE){
+                                if ($i == 0 && $j == 0 && $czy_wypisac_domain == "FALSE"){
                                     echo $item[$i][$j].";";
                                 }
-                                elseif($i > 0 && $j == 0 && $czy_wypisac_domain == FALSE){
+                                elseif($i > 0 && $j == 0 && $czy_wypisac_domain == "FALSE"){
                                     echo ";";
                                 }
-                                elseif ($j == 1 && $czy_wypisac_domain){
+                                elseif ($j == 1 && $czy_wypisac_domain == "TRUE"){
                                     echo $item[$i][$j].";";
                                 }else{
                                     if($j==0){
@@ -42,8 +42,8 @@ function domyslny ($ilosc_powt_pierw_kolumny,$ile_wierszy_wypisac,$czy_wypisac_d
                                     }
                                 }
                             }
-                        }else{
-                            if($i == 0 && $j == 0 && $czy_wypisac_domain){
+                        }elseif($czy_usunac == "FALSE"){
+                            if($i == 0 && $j == 0 && $czy_wypisac_domain == "TRUE"){
                                 echo "domain:".$item[$i][$j].";";
                             }else {
                                 echo $item[$i][$j].";";
@@ -53,9 +53,8 @@ function domyslny ($ilosc_powt_pierw_kolumny,$ile_wierszy_wypisac,$czy_wypisac_d
                 }
                 echo "\n";                 
             }
-            header('Content-Disposition: attachment;filename="'.$filename.'";');
         }elseif (count($item) <= $ilosc_powt_pierw_kolumny){
-            wypisz_wszystkie_wiersze_normalnie($item,$kolumny,$filename);
+            wypisz_wszystkie_wiersze_normalnie($item,$kolumny);
         }
     }
     
--- index.php
+++ index.php
@@ -1,147 +1,31 @@
 <?php
-//TO DO LIST
-// - dodać czytanie ile jest max kolumn
-// -
-
+//bez tego problem z nagłówkami
 ob_start();
-require_once 'functions.php';
-//max available uploaded file size in MB
-$max_file_size = 1;
-
-$kolumny = ((isset($_POST['kolumny']) && !empty($_POST['kolumny']))? $_POST['kolumny'] : 21);
-
-//available extensions
-$mimes = array('csv','txt');
-$mimes_print = '';
-
-//storing available extensions in var to display later
-foreach($mimes as $ext){ $mimes_print .= $ext." ";}
-
-//if the file exist
-if (isset($_FILES['plik'])){  
-    if ($_FILES['plik']['size'] <= ($max_file_size)*1024*1024){
-        switch($_FILES['plik']['error']){
-            case 0:{
-                if(isset($_FILES['plik']['name']) && !empty($_FILES['plik']['name'])){
-                    $uploaded_file_name = filter_var($_FILES['plik']['name'], FILTER_SANITIZE_STRING);
-                    $uploaded_file_ext = pathinfo($uploaded_file_name, PATHINFO_EXTENSION);   
-                    if(in_array($uploaded_file_ext,$mimes) ) {
-
-                        //wczytuje całą zawartośc do zmiennej i exploduje (dziele na tablice) ją po znaku konca linii
-                        $content = explode("\n", file_get_contents($_FILES['plik']['tmp_name']));
-
-                        //dodaje zmienna przechowujaca tablice
-                        $tab = array();
-                        
-                        //usuwam znak konca lini z kazdego elementu w tablicy
-                        $content = str_replace(array("\r", "\n"), '', $content);
-
-                        //pętla która wykonuje coś dla każdego elementu w tablicy
-                        foreach($content as $line){
-                                //przypisuje do zmiennej exp zawartość elementów i exploduje po średnikach
-                                $exp = explode(";", $line);
-                                //przypisuje zmiennej $tab alias oraz zawartosc talicy exp
-                                $tab[$exp[0]][] = $exp;
-                        }
-                        
-                        $new_file = ((isset($_POST['new_file']) && !empty($_POST['new_file']))? (filter_var($_POST['new_file'], FILTER_SANITIZE_STRING)) : "min_".$uploaded_file_name);
-                        
-                        //jesli sosob minifikacji - domyślny
-                        if(isset($_POST['domyslny'])){
-                            if(isset($_POST['ilosc_powt']) && !empty($_POST['ilosc_powt']) && isset($_POST['ile_wierszy']) && !empty($_POST['ile_wierszy'])){
-                                if($_POST['ilosc_powt'] >= $_POST['ile_wierszy']){
-                                    domyslny($_POST['ilosc_powt'], $_POST['ile_wierszy'], $_POST['czy_wypisac_domain'], $kolumny, $tab, $_POST['czy_usunac_duplikaty'],$new_file);
-                                }else{
-                                    echo "<div class='alert alert-danger'>Ilość wystąpień zawartości nie może być mniejsza od ilości wierszy które chcesz wyświetlić!</div>";
-                                }
-                            }else{
-                                domyslny(4, 1, TRUE, $kolumny, $tab, FALSE,$new_file);
-                            }
-                            
-
-                        }else{
-                            foreach ($tab as $item){
-                                wypisz_wszystkie_wiersze_normalnie($item, $kolumny,$new_file);
-                            }
-                        }
-                        //$zapis = trim($zapis,'');
-                        //zapis do nowego pliku
-                        //$f = fopen("php://memory", "w");
-                        //fputcsv($f, array($zapis));
-                        // reset the file pointer to the start of the file
-                       // rewind($f);
-                        // tell the browser it's going to be a csv file
-                        //header('Content-Type: application/csv; charset=UTF-8');
-                        // tell the browser we want to save it instead of displaying it
-                        //$new_file = ((isset($_POST['new_file']) && !empty($_POST['new_file']))? (filter_var($_POST['new_file'], FILTER_SANITIZE_STRING)) : "min_".$uploaded_file_name);
-                        //header('Content-Disposition: attachment;filename="'.$new_file.'";');
-                        // make php send the generated csv lines to the browser
-                       // fpassthru($f);
-                        //fclose($f);
-                       exit;
-
-                    }else{
-                        echo "<div class='alert alert-danger'>Niedozwolone rozszerzenie piku!</div>";
-                        echo "<div class='alert alert-info'>Dozwolone rozszerzenia pliku: <strong>".$mimes_print."</strong>.</div>";
-                    }
-                }else{
-                    echo "Plik który chcesz przesłać musi mieć nazwę!";
-                }
-//                echo "<div class='alert alert-success'>Wczytano plik: <strong>".$_FILES['plik']['name']."</strong>.<br>File size: <strong>".round((($_FILES['plik']['size'])/1024), 0)."</strong> KB</div>";
-//                echo "<pre>";
-//                echo var_dump($_FILES['plik']);
-//                echo "</pre>";
-//                break;
-            }
-            case 1:{
-                echo "<div class='alert alert-danger'>Za duży plik (php.ini)";
-            break;
-            }
-            case 2:{
-                echo "<div class='alert alert-danger'>Zbyt duży plik <strong>".$max_file_size_uploaded."</strong> KB.</div>";
-                echo "<div class='alert alert-info'>Dozwolona wielkość pliku: <strong>".$max_file_size."</strong> KB.</div>";
-                break;
-            }
-            case 3:{
-                echo "<div class='alert alert-danger'>Uszkodzony plik <strong>".$uploaded_file_name."</strong>.</div>";
-                break;
-            }
-            case 4:{
-                echo "<div class='alert alert-danger'>Nie wybrano pliku!</div>";
-                break;
-            }
-            default:{
-                echo "<div class='alert alert-danger'>Błąd!</div>";
-            }
-        }
-     }else{
-         echo "<div class='alert alert-danger'>Zbyt duży plik <strong>".$uploaded_file_name."</strong>.</div>";
-         echo "<div class='alert alert-info'>Dozwolona wielkość pliku: <strong>".$max_file_size."</strong> KB.<br>Wielkość przesłanego pliku: <strong>".round((($_FILES['plik']['size'])/1024), 0)."</strong> KB</div>";
-     }
-     }
+require_once 'main_minifier.php';
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>.csv Minifier</title>
         <link rel="stylesheet" href="css/bootstrap.min.css">
-        <style>
-            .small {
-                height: 0px;
-                overflow:hidden;
-            }
-            .big {
-                height: auto;
-            }
-            .wrapper a {
-                float: right;
-            }
-        </style>
+        <link rel="stylesheet" href="css/styles.css">
     </head>
     <body>
         <div class="container-fluid">
+            <h1>Minifier .csv</h1>
+            <h2>Jak korzystać?</h2>
+            <ol>
+                <li>Wybierz plik do obróbki.</li>
+                <li>Podaj opcjonalnie ile kolumn chcesz wyświetlić oraz nową nazwę pliku.</li>
+                <li>Jeśli nie zaznaczysz sposobu minifikacji możesz zmienić jedynie nazwę pliku lub ilość kolumn.</li>
+                <li>Zaznaczając <i>"Domyślny"</i> pod opisem pojawi się <i>"Personalizuj"</i>, gdzie masz możliwośc edycji.</li>
+                <li>Jeśli nie wpiszesz żadnych wartości w pola <i>"Ilość wystąpień"</i> oraz <i>"Ile wierszy"</i> skrypt wykona domyślną minifikację</li> 
+            </ol>
+            <div class="row" style="margin:20px;">
+            <span class="label label-warning"><strong>Uwaga!</strong> Skrypt porównuje jedynie pierwszą kolumnę w poszukiwaniu duplikatów. Upewnij się, że twój plik .csv lub .txt ma prawidłową konstrukcję!</span>
+            </div>
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <form action="index.php" method="post" enctype="multipart/form-data">
                         
                             <input type="hidden" name="MAX_FILE_SIZE" value="<?php ($max_file_size)*1024;?>">
@@ -195,6 +79,9 @@ if (isset($_FILES['plik'])){
                                             <div class="form-group row well">
                                                 <strong>Ilość wystąpień:</strong> podajesz od ilu wystąpień zawartości pierwszej kolumny skrypt ma zacząc działać.<br>
                                                 <strong>Ile wierszy:</strong> podajesz ile wierszy skrypt ma wyświetlić kiedy napotka powtarzający sie rekord.<br>
+                                                <img id="img_help1" class="initiallyHidden" src="img/help_img1.jpg"/>
+                                                <a id="img_help1">Pokaż pomoc</a>                                                   
+                                                
                                             </div>
                                             <div class="col-xs-4">
                                                 <label class="control-label " for="ilosc_powt">
@@ -226,6 +113,8 @@ if (isset($_FILES['plik'])){
                                                 </select>
                                                 <span class="help-block" id="hint_czy_wypisac_domain">
                                                     np:<br/><strong>domain:</strong>buty.pl; www.buty.pl/japonki;<br/>buty.pl; www.buty.pl/klapki;
+                                                <img id="img_help2" class="initiallyHidden" src="img/help_img2.jpg"/>
+                                                <a id="img_help2">Pokaż pomoc</a>  
                                                 </span>
                                             </div>    
                                         </div>
@@ -256,28 +145,7 @@ if (isset($_FILES['plik'])){
         </div>
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script>
-    $('.wrapper').find('a[href="#"]').on('click', function (e) {
-    e.preventDefault();
-    this.expand = !this.expand;
-    $(this).text(this.expand?"Zwiń":"Personalizuj");
-    $(this).closest('.wrapper').find('.small, .big').toggleClass('small big');
-    });
-    function showMe (box) {
-
-        var chboxs = document.getElementsByName("domyslny");
-        var vis = "none";
-        for(var i=0;i<chboxs.length;i++) { 
-            if(chboxs[i].checked){
-             vis = "block";
-                break;
-            }
-        }
-        document.getElementById(box).style.display = vis;
-
-
-    }
-    </script> 
+    <script src="js/script.js" async></script>
     </body>
 <?php
 ob_end_flush(); //wyrzuc html
